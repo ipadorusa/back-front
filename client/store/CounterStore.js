bd466f3a2b084ec0b432711289b7d6876c8ae1ba
ipadorusa <ipadorusa@gmail.com>
2023-04-06 17:05:27 +0900

    change

--- client/store/CounterStore.js
+++ client/store/CounterStore.js
@@ -1,18 +1,12 @@
 import { create } from 'zustand'
-import zukeeper from 'zukeeper'
 
 const initialState = {
   count: 0
 }
-const CounterStore = create(
-  zukeeper((set) => ({
-    ...initialState,
-    increment: () => set((state) => ({ count: state.count + 1 })),
-    decrement: () => set((state) => ({ count: state.count - 1 })),
-    resetCounterStore: () => set(initialState)
-  }))
-)
 
-window.store = CounterStore
-
-export default CounterStore
+export const CounterStore = create((set) => ({
+  ...initialState,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  resetCounterStore: () => set(initialState)
+}))
