33100e7c3a39b89ba3771d42d313b0900fcee07b
ipadorusa <ipadorusa@gmail.com>
2023-11-10 22:28:13 +0900

    month movie

--- client/src/pages/[year]/[month]/index.jsx
+++ client/src/pages/[year]/[month]/index.jsx
@@ -3,28 +3,33 @@ import { useRouter } from 'next/router';
 
 const YearMonth = () => {
   const router = useRouter();
-  const { year, month } = router.query;
-  const [useDate, setUseDate] = useState(new Date(parseInt(year), parseInt(month) - 1, 1));
+  const { year: routeYear, month: routeMonth } = router.query;
+  const [useDate, setUseDate] = useState(new Date(parseInt(routeYear), parseInt(routeMonth) - 1, 1));
 
   useEffect(() => {
-    setUseDate(new Date(parseInt(year), parseInt(month) - 1, 1));
-  }, [year, month]);
+    setUseDate(new Date(parseInt(routeYear), parseInt(routeMonth) - 1, 1));
+  }, [routeYear, routeMonth]);
 
   const prevMonth = () => {
     const newDate = new Date(useDate);
     newDate.setMonth(newDate.getMonth() - 1);
-    const newYear = newDate.getFullYear();
-    const newMonth = newDate.getMonth() + 1; // Months are zero-based
-
-    router.push(`/[year]/[month]`, `/${newYear}/${newMonth}`);
+    updateRoute(newDate);
   };
 
   const nextMonth = () => {
     const newDate = new Date(useDate);
     newDate.setMonth(newDate.getMonth() + 1);
-    const newYear = newDate.getFullYear();
-    const newMonth = newDate.getMonth() + 1; // Months are zero-based
+    updateRoute(newDate);
+  };
 
+  const goToToday = () => {
+    const today = new Date();
+    updateRoute(today);
+  };
+
+  const updateRoute = (newDate) => {
+    const newYear = newDate.getFullYear();
+    const newMonth = newDate.getMonth() + 1;
     router.push(`/[year]/[month]`, `/${newYear}/${newMonth}`);
   };
 
@@ -32,9 +37,10 @@ const YearMonth = () => {
     <div>
       <button type='button' onClick={prevMonth}>prev</button>
       <button type='button' onClick={nextMonth}>next</button>
+      <button type='button' onClick={goToToday}>Go to Today</button>
       <p>{useDate.toLocaleDateString()}</p>
-      <h1>Year: {year}</h1>
-      <h2>Month: {month}</h2>
+      <h1>Year: {routeYear}</h1>
+      <h2>Month: {routeMonth}</h2>
     </div>
   );
 };
